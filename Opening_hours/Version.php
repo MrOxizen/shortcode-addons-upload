650a323294f2cc765685f688b6cfec55698d576a
sabibur532 <53510812+sabibur532@users.noreply.github.com>
2019-10-22 13:39:47 +0600

    opening hours

--- Opening_hours/Version.php
+++ Opening_hours/Version.php
@@ -2,6 +2,7 @@
 /*
   Plugin Name: Shortcode Addons
   Version: 2.0.0
+  Edit & Checked By Sabibur Rahman
  */
 
-return array('2.0.0', 'Marketing Elements', true);
+return array('2.0.0', 'Marketing Elements', true, 'fas fa-history');
