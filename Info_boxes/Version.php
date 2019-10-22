3c9669fe72924f8640a660f266283542191f4e02
Richardsetu <richardsetu1@gmail.com>
2019-10-22 11:27:00 +0600

    dgfsdg

--- Info_boxes/Version.php
+++ Info_boxes/Version.php
@@ -2,6 +2,7 @@
 /*
   Plugin Name: Shortcode Addons
   Version: 1.5
+ * Checked by Richard
  */
 
-return array('2.0.0', 'Content Elements', true);
+return array('2.0.0', 'Content Elements', true, 'fas fa-info');
