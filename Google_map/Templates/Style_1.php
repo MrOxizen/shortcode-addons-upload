c062271f7fab347e12bd2697891e55f79ed70152
sabibur532 <53510812+sabibur532@users.noreply.github.com>
2019-12-03 15:37:52 +0600

    dd

--- Google_map/Templates/Style_1.php
+++ Google_map/Templates/Style_1.php
@@ -16,21 +16,90 @@ use SHORTCODE_ADDONS\Core\Templates;
 
 class Style_1 extends Templates {
 
-     public function default_render($style, $child, $admin) {
-       
+    public function default_render($style, $child, $admin) {
         
+        $oxiid = $this->oxiid;
+        echo '<div class="map-position-style1 map-position-' . $oxiid . '">
+                  <div  class="map-style1" id="map-' . $oxiid . '"></div>
+                </div>
+         ';
     }
 
     public function inline_public_jquery() {
-        
+        $key = $title = $lat = $lng = "";
+        if ($styledata[63] != "" && $styledata[59] != "" && $styledata[61] != "") {
+            if ($styledata[63] !== 'AIzaSyDd40qP9Qll71WJ0IBZtUrtAs--klcYLNo') {
+                $key = $styledata[63];
+                $lat = $styledata[59];
+                $lng = $styledata[61];
+            } else {
+                $key = "AIzaSyDd40qP9Qll71WJ0IBZtUrtAs--klcYLNo";
+                $lat = 1.361827;
+                $lng = 103.853342;
+                //echo '<script type="text/javascript">alert("Please give your  correct APi key.")</script>';
+            }
+        } else {
+            $key = "AIzaSyDd40qP9Qll71WJ0IBZtUrtAs--klcYLNo";
+            $lat = 1.361827;
+            $lng = 103.853342;
+            //echo '<script type="text/javascript">alert("Please give your correct APi key, latitude and longitude. Otherwise always show your default data.")</script>';
+        }
+        $title = OxiAddonsTextConvert($stylefiles[5]);
+        $oxiid = $this->oxiid;
+        $jquery = '';
+        $jquery .= ' <script>
+                  function initMap() {
 
-        $jquery .= "";
+                    var uluru = { lat: ' . $lat . ', lng: ' . $lng . ' };
+                    var map = new google.maps.Map(
+                      document.getElementById("map' . $oxiid . '"), { zoom: ' . $styledata[57] . ', center: uluru }
+                    );
+                    marker = new google.maps.Marker({
+                      map: map,
+                      position: uluru,
+                      title: "' . OxiAddonsTextConvert($stylefiles[5]) . '",
+                      animation: google.maps.Animation.DROP
+                    });
 
+
+                    var contentString =`<div class="oxi_add_gmap_location_info-style1 oxi_add_gmap_location_info-' . $oxiid . '">
+                                          <div class="oxi_add_gmap_location_info_body-style1 oxi_add_gmap_location_info_body-' . $oxiid . '">' . OxiAddonsTextConvert($stylefiles[7]) . '</div>
+                                       </div>
+                                          `;
+
+
+
+                    var infoWindow = new google.maps.InfoWindow({
+                      content: contentString
+                    })
+                    marker.addListener("click", function () {
+                      infoWindow.open(map, marker);
+                    });
+
+                    marker.addListener("click", toggleBounce);
+
+                  }
+
+                  function toggleBounce() {
+                      if(' . $styledata[65] . ' == 1){
+                        if (marker.getAnimation() !== null) {
+                          marker.setAnimation(null);
+                        } else {
+                          marker.setAnimation(google.maps.Animation.BOUNCE);
+                        }
+                      }else{
+                          marker.setAnimation(null);
+                      }
+                  }
+
+                </script>';
+
+        $jquery .= '<script src="https://maps.googleapis.com/maps/api/js?key=' . $key . '&callback=initMap"  async defer></script>';
+
+  
 
         return $jquery;
     }
-
-    
 
     public function old_render() {
         $styledata = $this->dbdata;
@@ -47,6 +116,8 @@ class Style_1 extends Templates {
             </div>
          </div>
         ';
+        
+        
         $key = $title = $lat = $lng = "";
         if ($styledata[63] != "" && $styledata[59] != "" && $styledata[61] != "") {
             if ($styledata[63] !== 'AIzaSyDd40qP9Qll71WJ0IBZtUrtAs--klcYLNo') {
