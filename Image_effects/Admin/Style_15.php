06260a4d25a2916a580f364a1c7121bd57af5ffa
Ashraful Fuad <48980302+ashrafulfuad@users.noreply.github.com>
2019-12-03 10:59:12 +0600

    gdfg

--- Image_effects/Admin/Style_15.php
+++ Image_effects/Admin/Style_15.php
@@ -217,17 +217,7 @@ class Style_15 extends AdminStyle {
                 ]
         );
 
-         $this->add_control(
-                'sa-ie-link-opening', $this->style, [
-            'label' => __('Link Opening', SHORTCODE_ADDOONS),
-            'type' => Controls::SWITCHER,
-            'loader' => TRUE,
-            'default' => 'yes',
-            'yes' => __('Yes', SHORTCODE_ADDOONS),
-            'no' => __('No', SHORTCODE_ADDOONS),
-            'return_value' => 'yes',
-                ]
-        );
+         
 
         $this->start_controls_tabs(
                 'shortcode-addons-start-tabs', [
@@ -757,32 +747,7 @@ class Style_15 extends AdminStyle {
             ],
                 ]
         );
-        $this->add_control(
-                'sa_ie_button_side_text_align', $this->style, [
-            'label' => __('Text Align', SHORTCODE_ADDOONS),
-            'type' => Controls::CHOOSE,
-            'operator' => Controls::OPERATOR_ICON,
-            'toggle' => TRUE,
-            'default' => 'center',
-            'options' => [
-                'left' => [
-                    'title' => __('Left', SHORTCODE_ADDOONS),
-                    'icon' => 'fa fa-align-left',
-                ],
-                'center' => [
-                    'title' => __('Center', SHORTCODE_ADDOONS),
-                    'icon' => 'fa fa-align-center',
-                ],
-                'right' => [
-                    'title' => __('Right', SHORTCODE_ADDOONS),
-                    'icon' => 'fa fa-align-right',
-                ],
-            ],
-            'selector' => [
-                '{{WRAPPER}} .sa_ie_temp_15 .oxi-addons-image-effects-button' => 'text-align: {{VALUE}};'
-            ],
-                ]
-        );
+        
         $this->add_group_control(
                 'sa-ie-button-side-animation', $this->style, [
             'type' => Controls::ANIMATION,
