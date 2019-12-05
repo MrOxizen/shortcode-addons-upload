5d6f59d0e3bcd50df8722521a72d23b2e5193c37
sabibur532 <53510812+sabibur532@users.noreply.github.com>
2019-12-05 15:27:35 +0600

    adsf

---
+++ Google_map/Admin/Style_3.php
@@ -0,0 +1,343 @@
+<?php
+
+namespace SHORTCODE_ADDONS_UPLOAD\Google_map\Admin;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Description of Style_1
+ * Content of Shortcode Addons Plugins
+ *
+ * @author $biplob018
+ */
+use SHORTCODE_ADDONS\Core\AdminStyle;
+use SHORTCODE_ADDONS\Core\Admin\Controls as Controls;
+
+class Style_3 extends AdminStyle {
+
+    public function register_controls() {
+
+        $this->start_section_tabs(
+                'shortcode-addons-start-tabs'
+        );
+
+        $this->start_section_devider();
+        $this->start_controls_section(
+                'shortcode-addons', [
+            'label' => esc_html__('Maps information', SHORTCODE_ADDOONS),
+            'showing' => TRUE,
+                ]
+        );
+        $this->add_repeater_control(
+                'sa_gm_repeater', $this->style, [
+            'label' => __('Edit or Delete your Maps information', SHORTCODE_ADDOONS),
+            'type' => Controls::REPEATER,
+            'fields' => [
+                 'sa_gm_name' => [
+                    'type' => Controls::TEXT,
+                    'label' => __('Name', SHORTCODE_ADDOONS),
+                    'placeholder' => __('Enter A name', SHORTCODE_ADDOONS),
+                    'default' => 'Location 01',
+                
+                ],
+                 'sa_gm_locatio_title' => [
+                    'type' => Controls::TEXT,
+                    'label' => __('Location Title', SHORTCODE_ADDOONS),
+                    'placeholder' => __('Location Title', SHORTCODE_ADDOONS),
+                    'default' => 'Title 01',
+                    'selector' => [
+                        '{{WRAPPER}} .oxi-hover-effects-styl1.oxi-hover-effects-styl1-{{KEY}} .oxi-hover-info .oxi-button-heading' => ''
+                    ],
+                ],
+                 'sa_gm_locatio_info' => [
+                    'type' => Controls::TEXTAREA,
+                    'label' => __('Location Information', SHORTCODE_ADDOONS),
+                    'placeholder' => __('Location Information', SHORTCODE_ADDOONS),
+                    'default' => '<h4>Google Map</h4> <hr> A very powerful Google Map implementation for Elementor with 8+ Map Types including Basic with Marker, Multiple Markers, Static, Polylines, Polygons, Overlay.',
+                    'selector' => [
+                        '{{WRAPPER}} .oxi-hover-effects-styl1.oxi-hover-effects-styl1-{{KEY}} .oxi-hover-info .oxi-button-heading' => ''
+                    ],
+                ],
+                'sa_gm_latitude' => [
+                    'type' => Controls::NUMBER,
+                    'label' => __('Latitude', SHORTCODE_ADDOONS),
+                    'default' => '1.361827',
+                    'step' => 0.1111
+                ],
+                'sa_gm_Longitude' => [
+                    'type' => Controls::NUMBER,
+                    'label' => __('Longitude', SHORTCODE_ADDOONS),
+                    'default' => '103.853342',
+                    'step' => 0.1111
+                ],
+            ],
+            'title_field' => 'sa_he_title_text',
+                ]
+        );
+        $this->end_controls_section();
+        $this->start_controls_section(
+                'shortcode-addons', [
+            'label' => esc_html__('Info Window', SHORTCODE_ADDOONS),
+            'showing' => TRUE,
+                ]
+        );
+        $this->add_group_control(
+                'sa_gm_Location_text_typho', $this->style, [
+            'type' => Controls::TYPOGRAPHY,
+            'include' => Controls::ALIGNNORMAL,
+            'selector' => [
+                '{{WRAPPER}} .oxi_add_gmap_location_info_body-style2' => ''
+            ],
+                ]
+        );
+        $this->add_control(
+                'sa_gm_location_text_color', $this->style, [
+            'label' => __('Color', SHORTCODE_ADDOONS),
+            'type' => Controls::COLOR,
+            'default' => '#dddddd',
+            'selector' => [
+                '{{WRAPPER}} .oxi_add_gmap_location_info_body-style2' => 'color:{{VALUE}};'
+            ],
+                ]
+        );
+        $this->add_responsive_control(
+                'sa_gm_location_width', $this->style, [
+            'label' => __('Info Window Width', SHORTCODE_ADDOONS),
+            'type' => Controls::SLIDER,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1000,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .oxi_add_gmap_location_info-style2' => 'width:{{SIZE}}{{UNIT}};'
+            ],
+                ]
+        );
+        $this->add_responsive_control(
+                'sa_gm_location_height', $this->style, [
+            'label' => __('Info Window Height', SHORTCODE_ADDOONS),
+            'type' => Controls::SLIDER,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1500,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .oxi_add_gmap_location_info-style2' => 'height:{{SIZE}}{{UNIT}};'
+            ],
+                ]
+        );
+        $this->end_controls_section();
+         $this->end_section_devider();
+
+        $this->start_section_devider();
+        $this->start_controls_section(
+                'shortcode-addons', [
+            'label' => esc_html__('General Setting', SHORTCODE_ADDOONS),
+            'showing' => TRUE,
+                ]
+        );
+        
+        $this->add_control(
+                'sa_gm_api', $this->style, [
+            'type' => Controls::TEXT,
+            'label' => __('API Key', SHORTCODE_ADDOONS),
+                ]
+        );
+
+        $this->add_responsive_control(
+                'sa_gm_width', $this->style, [
+            'label' => __('Width', SHORTCODE_ADDOONS),
+            'type' => Controls::SLIDER,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1000,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .map-style2' => 'max-width:{{SIZE}}{{UNIT}};'
+            ],
+                ]
+        );
+        $this->add_responsive_control(
+                'sa_gm_height', $this->style, [
+            'label' => __('Height', SHORTCODE_ADDOONS),
+            'type' => Controls::SLIDER,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 1500,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .map-style2' => 'height:{{SIZE}}{{UNIT}};'
+            ],
+                ]
+        );
+         $this->add_control(
+                'sa_gm_place_zoom', $this->style, [
+            'type' => Controls::NUMBER,
+            'label' => __('Zoom Level', SHORTCODE_ADDOONS),
+            'loader' => TRUE,
+            'default' => 14,
+                ]
+        );
+        $this->add_control(
+                'sa_gm_color_theme', $this->style, [
+            'label' => __('Color Themes', SHORTCODE_ADDOONS),
+            'type' => Controls::SELECT,
+            'default' => 'sa_gm_theme_color_silver',
+            'loader' => TRUE,
+            'options' => [
+                'sa_gm_theme_color_standard' => __('Standard', SHORTCODE_ADDOONS),
+                'sa_gm_theme_color_silver' => __('Silver', SHORTCODE_ADDOONS),
+                'sa_gm_theme_color_retro' => __('Retro', SHORTCODE_ADDOONS),
+                'sa_gm_theme_color_dark' => __('Dark', SHORTCODE_ADDOONS),
+                'sa_gm_theme_color_night' => __('Night', SHORTCODE_ADDOONS),
+                'sa_gm_theme_color_aubergine' => __('Aubergine', SHORTCODE_ADDOONS),
+            ],
+                ]
+        );
+        $this->add_responsive_control(
+                'sa_gm_padding', $this->style, [
+            'label' => __('Padding', SHORTCODE_ADDOONS),
+            'type' => Controls::DIMENSIONS,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                '%' => [
+                    'min' => 0,
+                    'max' => 50,
+                    'step' => .1,
+                ],
+                'px' => [
+                    'min' => -200,
+                    'max' => 200,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 10,
+                    'step' => .1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .map-position-style2' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};'
+            ],
+                ]
+        );
+        $this->add_responsive_control(
+                'sa_gm_margin', $this->style, [
+            'label' => __('Margin', SHORTCODE_ADDOONS),
+            'type' => Controls::DIMENSIONS,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                '%' => [
+                    'min' => 0,
+                    'max' => 50,
+                    'step' => .1,
+                ],
+                'px' => [
+                    'min' => -200,
+                    'max' => 200,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 10,
+                    'step' => .1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .map-position-style2' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};'
+            ],
+                ]
+        );
+        $this->add_group_control(
+                'sa_gm_animation', $this->style, [
+            'type' => Controls::ANIMATION,
+                ]
+        );
+
+        $this->end_controls_section();
+       
+       
+
+        
+        $this->end_section_devider();
+
+        $this->end_section_tabs();
+    }
+
+}
