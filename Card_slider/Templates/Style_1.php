814f340007b5f5b17cf120d3106c3efdf8118c78
Richardsetu <richardsetu1@gmail.com>
2020-01-16 14:47:15 +0600

    sdfsaf

--- Card_slider/Templates/Style_1.php
+++ Card_slider/Templates/Style_1.php
@@ -30,31 +30,7 @@ class Style_1 extends Templates
     }
 
 
-    function inline_public_css()
-    {
-        $style = $this->style;
-        $hoverData = $css_inline = $final = '';
-        if ($style['sa_post_carousel_meta_position_effect'] == 'left') {
-            $css_inline = 'transform: translateX(-100%)';
-            $hoverData = 'transform: translateX(0)';
-        } elseif ($style['sa_post_carousel_meta_position_effect'] == 'top') {
-            $css_inline = 'transform: translateY(-100%)';
-            $hoverData = 'transform: translateY(0)';
-        } elseif ($style['sa_post_carousel_meta_position_effect'] == 'right') {
-            $css_inline = 'transform: translateX(100%)';
-            $hoverData = 'transform: translateX(0)';
-        } elseif ($style['sa_post_carousel_meta_position_effect'] == 'bottom') {
-            $css_inline = 'transform: translateY(100%)';
-            $hoverData = 'transform: translateY(0)';
-        }
-        $final = ' .' . $this->WRAPPER . ' .oxi-addons__main-wrapper-style-2 .oxi-addons__overlay{
-                        ' . $css_inline . '
-                    }
-                    .' . $this->WRAPPER . ' .oxi-addons__main-wrapper-style-2 .oxi-addons__main-img:hover .oxi-addons__overlay{
-                        ' . $hoverData . '
-                    }';
-        return $final;
-    }
+    
     public function post_render()
     {
         $style = $this->style;
@@ -109,24 +85,11 @@ class Style_1 extends Templates
 
                 if ($style['sa_s_image_layout_show_image'] == 'show') {
                     if ($image_url[0] != '') {
-                        if ($style['sa_post_carousel_img_eq_height'] == 'true') {
-                            $ssstyle .= '
-                                    background-position: center center;
-                                    background-repeat: no-repeat;
-                                    background-size: cover;
-                                    padding-bottom:' . $style['sa_post_carousel_thumbnail_height'] . '%;';
-                        }
                         $img = ' <a class="oxi-addons__post-link" href="' . get_permalink($query->post->ID) . '"  target="' . $style['sa_s_image_layout_linke_open'] . '">
-                                <div class="oxi-addons__main-img oxi-addons__main-img_' . $query->post->ID . '" style=" background-image: url(' . $image_url[0] . '); ' . $ssstyle . '">';
-
-                        if ($style['sa_post_carousel_img_eq_height'] != 'true') {
-                            $img .= '<img class="oxi-image" src="' . $image_url[0] . '">';
-                        }
-                        $img .= ' <div class="oxi-addons__overlay">
-                                        ' . $this->font_awesome_render($style['sa_post_carousel_icon']) . '
-                                    </div>
-                                </div>
-                            </a>
+                                    <div class="oxi-addons__main-img oxi-addons__main-img_' . $query->post->ID . '">
+                                        <img class="oxi-image" src="' . $image_url[0] . '">
+                                     </div>
+                                 </a>
                         ';
                     }
                 }
@@ -245,15 +208,15 @@ class Style_1 extends Templates
             ';
         }
         $rtl = (array_key_exists('sa_post_carousel_direction', $style) && $style['sa_post_carousel_direction'] == 'right') ? 'dir="rtl"' : '';
-        echo '<div class="oxi-addons__post-carousel-style-2 swiper-container-wrap-dots-' . $style['dots_position'] . '">
+        echo '<div class="oxi-addons__card-slider-style-1 swiper-container-wrap-dots-' . $style['dots_position'] . '">
                     <div class="swiper-container oxi-addons__post-carousel-wrap ' . $style['sa_post_carousel_image_switcher'] . '" ' . $rtl . '>
                         <div class="swiper-wrapper">
                             ' . $this->post_render() . '
-                        </div>
-                    </div>';
+                        </div>';
+                    echo '</div>';
         if (array_key_exists('sa_post_carousel_dots', $style) && $style['sa_post_carousel_dots'] == 'yes') :
-            echo '<div class="swiper-pagination sa_post_carousel_pagination_' . $this->oxiid . '"></div>';
-        endif;
+                            echo '<div class="swiper-pagination '.$style['dots_types'].' sa_post_carousel_pagination_' . $this->oxiid . '"></div>';
+                        endif;
         echo $arrow;
         echo '</div>
             ';
@@ -278,7 +241,7 @@ class Style_1 extends Templates
         $grab_cursor = (array_key_exists('sa_post_carousel_grab_cursor', $style) && $style["sa_post_carousel_grab_cursor"] == "yes") ? "1" : "0";
         $auto_height = $style["sa_post_carousel_auto_height"];
         $js = '';
-        $js = 'var sa_post_carousel = $(".' . $this->WRAPPER . ' .oxi-addons__post-carousel-style-2 .oxi-addons__post-carousel-wrap");
+        $js = 'var sa_post_carousel = $(".' . $this->WRAPPER . ' .oxi-addons__card-slider-style-1 .oxi-addons__post-carousel-wrap");
             
             var saPostCarousel = new Swiper(sa_post_carousel, {
                 direction: "horizontal",
